11d76ee30bee5a0335235752d097e25711e86495
adii2710 <adityakhopade2003@gmail.com>
2024-06-25 18:26:29 +0530

    updated numpy version in requirements.txt

--- crop_img.py
+++ crop_img.py
@@ -1,17 +1,4 @@
-# from rembg import remove
-
-# class RemImgBackground:
-#     def __init__(self):
-#         self.img=None
-#         self.out_img=None
-    
-#     def remBg(self, img):
-#         self.img=img
-#         out=remove(self.img)
-#         # out.save('images/out.png')
-#         return out
-
-import cv2
+from rembg import remove
 
 class RemImgBackground:
     def __init__(self):
@@ -20,16 +7,6 @@ class RemImgBackground:
     
     def remBg(self, img):
         self.img=img
-        # Create a BackgroundSubtractor
-        bg_subtractor = cv2.createBackgroundSubtractorMOG2()
-
-        # Apply the BackgroundSubtractor
-        fg_mask = bg_subtractor.apply(self.img)
-
-        # Create a mask by thresholding the foreground mask
-        _, mask = cv2.threshold(fg_mask, 127, 255, cv2.THRESH_BINARY)
-
-        # Apply the mask to the image
-        result = cv2.bitwise_and(self.img, self.img, mask=mask)
-
-        return result
+        out=remove(self.img)
+        # out.save('images/out.png')
+        return out
--- symmetry.py
+++ symmetry.py
@@ -1,11 +1,8 @@
 import streamlit as st
 from PIL import Image
 from crop_img import RemImgBackground
-import matplotlib.pyplot as plt
 from symmetry_module import SymmetryAnalysis
-import cv2
 import mail
-import numpy as np
 
 def resizeImgs(img1, img2):
     #get the size of img 
@@ -61,7 +58,6 @@ if view_type == "Front/Rear View":
     # st.write(type(np.asarray(front_rear_image)))
     if front_rear_image:
         front_rear_image = Image.open(front_rear_image)
-        front_rear_image=np.array(front_rear_image)
         front_rear_image=rembackground.remBg(front_rear_image)
         
 elif view_type == "Side View":
@@ -70,10 +66,8 @@ elif view_type == "Side View":
     side_image2 = st.file_uploader("Upload Left Side Image", type=["jpg", "jpeg", "png"])
     if side_image1 and side_image2:
         side_image1 = Image.open(side_image1)
-        side_image1 = np.array(side_image1)
         side_image1 = rembackground.remBg(side_image1)
         side_image2 = Image.open(side_image2)
-        side_image2 = np.array(side_image2)
         side_image2 = rembackground.remBg(side_image2)
         side=1
         side_image1, side_image2=resizeImgs(side_image1, side_image2)
@@ -84,6 +78,7 @@ images=[]
 symmetry_percent=''
 # Button to trigger analysis
 if st.button("Perform Symmetry Analysis"):
+    # if front_rear_image or (side_image1 and side_image2):
     if front_rear_image or (side_image1 and side_image2):
         analysis = SymmetryAnalysis(front_rear_image, side_image1, side_image2, side=1 if view_type == "Side View" else 0, base=base)
         st.session_state.analysis_results = analysis.calcSymmetry()
